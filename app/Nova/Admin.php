447ba1a2a4b0bffd107d17a1efcd829d47f070eb
ELONE AIMERICK <eloneaimerick2001@gmail.com>
2025-07-12 10:21:37 +0000

    maj2

--- app/Nova/Admin.php
+++ app/Nova/Admin.php
@@ -22,7 +22,7 @@ class Admin extends Resource
      *
      * @var string
      */
-    public static $title = 'name';
+    public static $title = 'nom';
 
     /**
      * The columns that should be searched.
@@ -30,7 +30,7 @@ class Admin extends Resource
      * @var array
      */
     public static $search = [
-        'id', 'name', 'email',
+        'id', 'nom', 'email',
     ];
 
     /**
